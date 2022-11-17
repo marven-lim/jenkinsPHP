7e5e99ffb130807dab1e57ea8c768d4e18ef470d
marven-lim <74282240+marven-lim@users.noreply.github.com>
2022-11-17 09:54:33 +0800

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,7 @@ $Username = 'isotope';
 $Password = '1s0t0p3';
 
 function PasswordIsValid($username, $password){
+	global $Username, $Password;
 	if ($username===$Username && $password===$Password){
 		return true;
 	}
